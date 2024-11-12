b24f73faaf38edf1fd1c3e15cba418c0581c3b56
Ted Ryu <fksak80@gmail.com>
2024-11-13 04:42:48 +0900

    1.0.0

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // baseURL: 'https://api.meetple.co.kr',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://api.meetple.co.kr',
+  // baseURL: 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
