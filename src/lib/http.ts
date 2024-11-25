849533d661a9365f528f895d0ae27b4bbe245617
Ted Ryu <fksak80@gmail.com>
2024-11-25 22:50:11 +0900

    api url update

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://api.meetple.co.kr',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
